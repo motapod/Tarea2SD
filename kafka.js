3b462e4a2976c73ddeccfd11b9ef6ce986c3d7cb
motapod <65835408+motapod@users.noreply.github.com>
2023-05-25 16:35:05 -0400

    .

--- kafka.js
+++ kafka.js
@@ -66,4 +66,62 @@ async function main() {
   }
 }
 
+const producer = kafka.producer()
+producer.on('producer.connect', () => {
+  console.log(`KafkaProvider: connected`);
+});
+producer.on('producer.disconnect', () => {
+  console.log(`KafkaProvider: could not connect`);
+});
+producer.on('producer.network.request_timeout', (payload) => {
+  console.log(`KafkaProvider: request timeout ${payload.clientId}`);
+});
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'supplier-ratings',
+    messages: [
+      {
+        value: Buffer.from(JSON.stringify(
+          {
+            "event_name": "QA",
+            "external_id": user_uuiD,
+            "payload": {
+              "supplier_id": i.supplier_id,
+              "assessment": {
+                "performance": 7,
+                "quality": 7,
+                "communication": 7,
+                "flexibility": 7,
+                "cost": 7,
+                "delivery": 6
+              }
+            },
+            "metadata": {
+              "user_uuid": "5a12cba8-f4b5-495b-80ea-d0dd5d4ee17e"
+            }
+          }
+        ))
+      },
+    ],
+  })
+
+  Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
+
 main();
